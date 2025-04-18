c2db86e37a8cc9525e71bef31c8ad45d0fed2d16
ABlankenship27 <C27Aidan.Blankenship@afacademy.af.edu>
2025-04-17 23:31:22 -0600

    finished

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -25,7 +25,14 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is
 
     -- signal declarations
-    
+    signal w_clk_slow : std_logic;
+    signal w_clk_fast : std_logic;
+    signal w_floor : std_logic_vector(3 downto 0) := (others => '0');
+    signal w_floor1 : std_logic_vector(3 downto 0) := (others => '0');
+    signal w_floor2 : std_logic_vector(3 downto 0) := (others => '0');
+    signal w_sel : std_logic_vector(3 downto 0)	:= (others => '0');
+    signal w_reset_clock : std_logic;
+    signal w_reset_fsm : std_logic;
   
 	-- component declarations
     component sevenseg_decoder is
@@ -70,14 +77,73 @@ architecture top_basys3_arch of top_basys3 is
 	
 begin
 	-- PORT MAPS ----------------------------------------
+	   sevenseg_decoder_port : sevenseg_decoder port map(
+	   i_Hex => w_floor,
+	   o_seg_n => seg
+	   );
+	
+    	elevator_controller_fsm_port : elevator_controller_fsm port map(
+    	i_clk => w_clk_slow,
+    	i_reset => w_reset_fsm,
+    	is_stopped => sw(0),
+    	go_up_down => sw(1),
+    	o_floor => w_floor1
+    	);
     	
+	   
+	   elevator_controller_fsm_port2 : elevator_controller_fsm port map(
+    	i_clk => w_clk_slow,
+    	i_reset => w_reset_fsm,
+    	is_stopped => sw(14),
+    	go_up_down => sw(15),
+    	o_floor => w_floor2
+    	);
+    	
+    	TDM4_port : TDM4 
+    	generic map ( k_WIDTH => 4 )
+    	port map(
+    	i_clk => w_clk_fast,
+    	i_reset => '0',
+    	i_D3 => x"F",
+    	i_D2 => w_floor2,
+    	i_D1 => x"F",
+    	i_D0 => w_floor1,
+    	o_data => w_floor,
+    	o_sel => w_sel
+    	);
+    	
+    	clkdiv_slow : clock_divider 		
+        generic map ( k_DIV => 50000000 ) 
+        port map (						  
+            i_clk   => clk,
+            i_reset => w_reset_clock,
+            o_clk   => w_clk_slow
+        ); 
+        
+        clkdiv_fast : clock_divider 		
+        generic map ( k_DIV => 400 ) 
+        port map (						  
+            i_clk   => clk,
+            i_reset => btnL,
+            o_clk   => w_clk_fast
+        ); 
 	
 	-- CONCURRENT STATEMENTS ----------------------------
+    an(0)   <= w_sel(0);
+    an(1)   <= w_sel(1);
+    an(2)   <= w_sel(2);
+    an(3)   <= w_sel(3);
 	
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
+	led(15) <= w_clk_slow;
+	led(14 downto 0) <= (others => '0');
 	
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- reset signals
+	 w_reset_clock <= btnL or btnU;
+	 w_reset_fsm <= btnR or btnU;
+	 
+	
 	
 end top_basys3_arch;
